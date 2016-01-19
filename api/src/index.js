2cf9dc82faf3a787d160f0e4c8c2e9de4cabca13
Michael M Kuhinica <taverneiro@gmail.com>
2016-01-19 22:42:33 +1300

    Deleted sample route

--- api/src/index.js
+++ api/src/index.js
@@ -77,13 +77,6 @@ var parseLogs = function(data) {
   return logs;
 };
 
-app.get('/', function(req, res, next) {
-  client.incr('counter', function(err, counter) {
-    if(err) return next(err);
-    res.send('This page has been viewed ' + counter + ' times!');
-  });
-});
-
 var getRoundedTimestamp = function(timestamp, granularity) {
   var factor = granularity.size * granularity.factor;
   return Math.floor(timestamp / factor) * factor;
